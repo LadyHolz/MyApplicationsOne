134747aac553a7fe1e7b607d4623bf1cdb1117bc
Gemma Holz <Gemma.Holz@balticapprenticeships.com>
2022-05-22 13:04:59 +0100

    Add project files.

---
+++ ConsoleAppOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleAppOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((num1 * num2)/2);
+
+
+        }
+    }
+}
